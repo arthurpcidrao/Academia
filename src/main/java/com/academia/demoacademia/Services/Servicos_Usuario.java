c64f45bb19e08e92cbad0086ebd93e8757513ed6
Arthur Paraiba Cidrao <arthurcidrao@sas.com.br>
2024-11-24 16:04:05 -0300

    Lógica de Autenticação de usuário implementada

--- src/main/java/com/academia/demoacademia/Services/Servicos_Usuario.java
+++ src/main/java/com/academia/demoacademia/Services/Servicos_Usuario.java
@@ -1,4 +1,32 @@
 package com.academia.demoacademia.Services;
 
+import com.academia.demoacademia.Model.Usuario;
+import com.academia.demoacademia.Repository.Usuario_DAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class Servicos_Usuario {
+
+    @Autowired
+    private Usuario_DAO repositorio;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public String autenticar_usuario(String login, String senha_bruta) {
+        Optional<Usuario> usuario = repositorio.findById(login);
+        boolean status;
+        if(usuario.isPresent()) {
+            status = this.passwordEncoder.matches(senha_bruta, usuario.get().getSenha());
+            if (status) return "Senha correta";
+
+            return "Senha incorreta";
+        }
+        return "Usuário não existe";
+    }
 }
